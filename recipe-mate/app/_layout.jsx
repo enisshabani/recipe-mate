31a161e8975bcd6ce8f3689962e7038f9d600e97
Lis Spahija <lispahija@gmail.com>
2025-12-18 20:27:54 +0100

    Remove forced authentication on app launch

--- recipe-mate/app/_layout.jsx
+++ recipe-mate/app/_layout.jsx
@@ -4,26 +4,16 @@ import { RecipeProvider } from "../contexts/RecipeContext";
 import { AuthProvider, useAuth } from "../contexts/AuthContext";
 
 function RootLayoutContent() {
-  const { isAuthenticated, loading } = useAuth();
-
-  // Show nothing while loading authentication state
-  if (loading) {
-    return null;
-  }
-
   return (
     <RecipeProvider>
       <Stack screenOptions={{ headerShown: false }}>
-        {isAuthenticated ? (
-          <>
-            <Stack.Screen name="(tabs)" />
-            <Stack.Screen name="add" />
-            <Stack.Screen name="recipe" />
-            <Stack.Screen name="editRecipe" />
-          </>
-        ) : (
-          <Stack.Screen name="(auth)" />
-        )}
+        <Stack.Screen name="(tabs)" />
+        <Stack.Screen name="(auth)" />
+        <Stack.Screen name="add" />
+        <Stack.Screen name="recipe" />
+        <Stack.Screen name="editRecipe" />
+        <Stack.Screen name="favorites" />
+        <Stack.Screen name="mealDetails" />
       </Stack>
     </RecipeProvider>
   );
